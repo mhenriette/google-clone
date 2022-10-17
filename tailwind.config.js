22b47b1f7ccb94f9df116bf142222789ebe65019
mhenriette <mhenriette00@gnmail.com>
2022-10-17 15:42:18 +0200

    first commit on dev

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [],
+    theme: {
+        extend: {
+            colors: {
+                textfooter: '#70757a',
+                footerbg: '#f2f2f2;',
+                bordergrey: '#dadce0;',
+                linkbg: '#f8f9fa;',
+                linktext: '#3c4043',
+                languagecolor: '#1a0dab;'
+
+
+            },
+        },
+    },
+    plugins: [],
+}
